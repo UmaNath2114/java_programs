2316afb5d9d84aec0fe5fa1f1de5426d1d3c8117
UmaNath2114 <govardhan2114@gmail.com>
2025-05-27 09:38:39 +0530

    Strings

---
+++ Desktop/java programs/EvenString.java
@@ -0,0 +1,17 @@
+public class EvenString {
+    // Java program to print even length words in a string
+        public static void printWords(String s)
+        {
+            for (String w : s.split(" ")){
+                // if length is even
+                if (w.length() % 2 == 0)
+                    System.out.println(w);
+            }
+        }
+
+        public static void main(String[] args)
+        {
+            String s = "i am belongs to ECE Branch";
+            printWords(s);
+        }
+    }
---
+++ Desktop/java programs/Prog6.java
@@ -0,0 +1,7 @@
+public class Prog6 {
+    public static void main(String []args){
+
+        String name = "ఉమానాథ్";
+        System.out.println("My name in telugu is : "+name);
+    }
+}
